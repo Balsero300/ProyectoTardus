38ff63c2f5e70fca4e030b03e3a6f3fb6b6e87f4
Balserr300 <lukitasantarelli@gmail.com>
2023-11-06 18:44:22 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const nombre = 'Luca'
+function App() {
+  //Estado
+  //Ciclo de vida
+  //jsx
+  return (
+    <div className="App">
+      <h1>Hola mundo {nombre}</h1>
+    </div>
+  )
+}
+
+export default App
