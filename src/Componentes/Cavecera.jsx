bcd173b83dcb6f78c261ee321fa791af28508251
Balserr300 <lukitasantarelli@gmail.com>
2023-11-09 17:07:16 -0300

    Agrego munu extendido

--- src/Componentes/Cavecera.jsx
+++ src/Componentes/Cavecera.jsx
@@ -1,14 +1,31 @@
+import { Menu } from 'style-components';
 import './Cavecera.css'
+import { useState } from 'react';
 
 function Cavecera (){
-    const estilo = {
-        background: 'red',
-    };
-    
+    const [menu , setMenu ] = useState(false)
+
+    function toggleMenu (){
+        setMenu( !menu )
+    }
+
     return (
-        <div style={estilo}>
-            Encabesado
-        </div>
+        <header className='header'>
+            <button onClick={ toggleMenu } className="encabezado-boton">
+                <svg class="encabezado-svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </button>
+            <nav className={ `emcabezado-nav ${ menu ? 'isActive' : '' }` }>
+                <ul className="encabezado-ul">
+                    <li className="encavezado-li"><a href="" className="encavezado-a">Item 1</a></li>
+                    <li className="encavezado-li"><a href="" className="encavezado-a">Item 2</a></li>
+                    <li className="encavezado-li"><a href="" className="encavezado-a">Item 3</a></li>
+                    <li className="encavezado-li"><a href="" className="encavezado-a">Item 4</a></li>
+                    <li className="encavezado-li"><a href="" className="encavezado-a">Item 5</a></li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
