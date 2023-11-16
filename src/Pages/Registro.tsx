3ca301330815e11bca3cc113bccd08967b5954d3
Balserr300 <lukitasantarelli@gmail.com>
2023-11-16 20:21:01 -0300

    Agrego nueva pantalla de regisro

---
+++ src/Pages/Registro.tsx
@@ -0,0 +1,7 @@
+function registro() {
+    return (
+      <h1>Hola</h1>
+   )
+}
+
+export default registro
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,7 +4,7 @@ import Inicio from '../Pages/Pagina'
 function Router() {
     return(
         <Routes>
-            <Route path='/login' element={<h1>Login de pagina</h1>} />
+            
             <Route path='/inicio' element={<Inicio/>} />
         </Routes>
     )
