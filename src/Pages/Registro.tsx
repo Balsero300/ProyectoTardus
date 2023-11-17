248e35a00e00fd0bcd692bbd8a9fd5912d3a6686
Balserr300 <lukitasantarelli@gmail.com>
2023-11-17 15:53:43 -0300

    Genero css para registro

--- src/Pages/Registro.tsx
+++ src/Pages/Registro.tsx
@@ -1,6 +1,11 @@
+import '../Componentes/Registro.css'
+
 function Registro() {
     return (
-        <></>
+        <div className="container">
+            <h1>Registro</h1>
+            
+        </div>
     )
 }
 
