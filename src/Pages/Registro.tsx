35c77e4a97f934252c63213cc3e122a85f2e2d6d
Balserr300 <lukitasantarelli@gmail.com>
2023-11-17 16:13:11 -0300

    Ingreso html de pagina

--- src/Pages/Registro.tsx
+++ src/Pages/Registro.tsx
@@ -1,10 +1,47 @@
 import '../Componentes/Registro.css'
+import '../Componentes/Inicio_S.css'
 
 function Registro() {
     return (
         <div className="container">
             <h1>Registro</h1>
-            
+            <form className="Form">
+                <label>
+                    <input className='Inp' type="text" placeholder='Nombre de Usuario' />
+                </label>
+                <br />
+                <label>
+                    <input className='Inp' type="text" placeholder='Correo Electronico' />
+                </label>
+                <br />
+                <label>
+                    <input className='Inp' type="password" placeholder='Contraseña' />
+                </label>
+                <br />
+                <label>
+                    <input className='Inp' type="password" placeholder='Confirmar Contraseña' />
+                </label>
+                <p>O</p>
+                <label>
+                    <input className='Inp' type="password" placeholder='Contraseña' />
+                </label>
+                <br />
+                <label>
+                    <input className='Inp' type="password" placeholder='Confirmar Contraseña' />
+                </label>
+                <br />
+                <input type="checkbox" id='terminos' />
+                <label for="terminos" >Acepto los terminos de Condiciones y servicios</label>
+            </form>
+            <p>¿Ya tienes una cuenta? <a href="#">Inicia Secion</a></p>
+            <div className="buttons-container">
+           <button className="Cancelar" type="button">
+             Cancelar
+           </button>
+           <button className="Inicio" type='button'>
+             Registrarse
+           </button>
+         </div>
         </div>
     )
 }
