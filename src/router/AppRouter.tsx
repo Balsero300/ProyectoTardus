cb2cf40c89265a254b8bee34e6ca98ad5b43a7fe
Balserr300 <lukitasantarelli@gmail.com>
2023-11-17 15:45:41 -0300

    Genero pantalla de registro y vinvulo con AppRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Inicio from '../Pages/Pagina'
 import Secion from '../Pages/Inicio_S'
+import Registro from '../Pages/Registro'
 
 function Router() {
     return(
         <Routes>
             <Route path='/secion' element={<Secion/>} />
             <Route path='/inicio' element={<Inicio/>} />
+            <Route path='/registro' element={<Registro/>}/>
         </Routes>
     )
 }
